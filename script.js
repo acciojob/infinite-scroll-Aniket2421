31808ea8a3317d566d13b8ad7d59c483ca008cce
Aniket2421 <115690076+Aniket2421@users.noreply.github.com>
2023-03-03 15:09:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-const list = document.getElementById('#infi-list');
-let page = 1;
-let isLoading = false;
+const list = document.getElementById('infi-list');
 
- function infinteScroll(){
-  const { scrollTop, clientHeight, scrollHeight } = list;
-  
-  // check if the user has scrolled to the bottom of the list
-  if (scrollTop + clientHeight >= scrollHeight && !isLoading) {
-    isLoading = true;
-    
-    // simulate a network request with a timeout
-    setTimeout(function() {
-      // create new li elements and append them to the list
-      for (let i = 0; i < 10; i++) {
-        const li = document.createElement('li');
-        li.textContent = `Item ${page * 10 + i + 1}`;
-        list.appendChild(li);
-      }
-      
-      isLoading = false;
-      page++;
-    }, 1000);
+list.addEventListener('scroll', function() {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    // user has reached the end of the list
+    const newItem1 = document.createElement('li');
+    newItem1.textContent = 'Item ' + (list.children.length + 1);
+    const newItem2 = document.createElement('li');
+    newItem2.textContent = 'Item ' + (list.children.length + 2);
+    list.appendChild(newItem1);
+    list.appendChild(newItem2);
   }
-};
-list.addEventListener('scroll',infinteScroll)
+});
